a482bf27f0a531fc21c0ec6993d49279bfae45eb
Dima <gubatenkod@gmail.com>
2017-05-23 16:22:20 +0300

    Initial Commit

---
+++ DisplayPlanStats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisplayPlanStats
+//
+//  Created by Dima Gubatenko on 23.05.17.
+//  Copyright © 2017 A2Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

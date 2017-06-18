3b058ef31636c2e228fb01ec02f17e05c31e88ca
Dima <gubatenkod@gmail.com>
2017-06-18 16:54:37 +0300

    update loadFromXib func

--- DisplayPlanStats/ChartCellProtocol.swift
+++ DisplayPlanStats/ChartCellProtocol.swift
@@ -16,8 +16,8 @@ protocol ChartCellProtocol {
 
 extension ChartCellProtocol {
 
-    static func loadFromXib(owner: Any?) -> Self? {
-        let xib = Bundle.main.loadNibNamed(String(describing: self), owner: owner, options: nil)?.first as? Self
+    static func loadFromXib(owner: Any?, options: [AnyHashable : Any]?) -> Self? {
+        let xib = Bundle.main.loadNibNamed(String(describing: self), owner: owner, options: options)?.first as? Self
         xib?.initialise()
         return xib
     }
